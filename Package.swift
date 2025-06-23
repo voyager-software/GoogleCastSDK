ba2e6a7f83d1ed18579e83acde82eb80c21d37d9
Gabor Shaio <gabor@voyagerweb.ca>
2025-06-23 17:55:21 -0600

    Update

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "GoogleCastSDK",
-            targets: ["GoogleCastSDK"]
+            name: "GoogleCast",
+            targets: ["GoogleCast"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "GoogleCastSDK",
+            name: "GoogleCast",
             url: "https://dl.google.com/dl/chromecast/sdk/ios/GoogleCastSDK-ios-4.8.3_dynamic.zip",
             checksum: "bc2c3c2434ef2895a0388ac3f16932242d3d3ac11805f810dbe7d7bce3bb27f6"
         ),
